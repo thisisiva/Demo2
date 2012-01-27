e6054461427ce0c02adddaa2c1d6c8e166f8ca9d
Iva Neykova <nivelina@yahoo.com>
2012-01-26 21:06:27 -0500

    tache 1: print message

---
+++ src/demo2/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo2;
+
+/**
+ *
+ * @author hd391378
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Premiere modification");
+    }
+}
