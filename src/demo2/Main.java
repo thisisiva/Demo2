c92f1ff5ad1fb2034c8860b786ce88a06bfb6dd7
Iva Neykova <nivelina@yahoo.com>
2012-01-26 21:42:40 -0500

    tache 2

--- src/demo2/Main.java
+++ src/demo2/Main.java
@@ -14,6 +14,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Premiere modification");
+        System.out.println("Deuxieme modification");
     }
 }
